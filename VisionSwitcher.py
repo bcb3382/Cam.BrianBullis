2f0efa378e31967944ac98b747f1a30571de8d71
Brian Bullis <35986244+bcb3382@users.noreply.github.com>
2019-04-06 01:36:18 -0400

    Documented VisionSwitcher.py

--- VisionSwitcher.py
+++ VisionSwitcher.py
@@ -14,36 +14,49 @@ try:
 except:
     print("Error importing.")
 
+# Make a sun
 sun = Sun()
+
+# The coordinates of the place to check sunrise and sunset
 coords = {'longitude' : 123, 'latitude' : 123} # enter the coords of your camera here
 
+# Calculate times to use in the comparisons for deciding which vision to use - THESE ARE IN UTC
 sunriseTime = sun.getSunriseTime(coords)['decimal'] * 3600
 sunsetTime = sun.getSunsetTime(coords)['decimal'] * 3600
 currentTimeTime = datetime.utcnow()
 currentTime = currentTimeTime.hour * 3600 + currentTimeTime.minute * 60 + currentTimeTime.second
 
+# Is it before sunrise?
 def beforeSunrise():
     return currentTime > sunriseTime
 
+# Is it after sunrise?
 def afterSunrise():
     return currentTime < sunriseTime
 
+# Is it before sunset?
 def beforeSunset():
     return currentTime > sunsetTime
 
+# Is it after sunset?
 def afterSunset():
     return currentTime < sunsetTime
 
+# If sunrise has passed but sunset has not, day vision should be active
 def dayVision():
     return afterSunrise() & beforeSunset()
 
+# If it is before sunrise or after sunset, night vision should be active
 def nightVision():
     return beforeSunrise() | afterSunset()
 
 if __name__ == '__main__':
+    # Call the DayVision script to make it day vision
     if dayVision():
         DayVision.main()
+    # Call the NightVision script to make it night vision
     if nightVision():
         NightVision.main()
+    # ¯\_(ツ)_/¯
     else:
         exit(0)
